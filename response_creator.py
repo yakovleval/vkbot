79e03f63d45eee363176f25800738d0acd4df5d6
alex <sanya89_muravlenko@mail.ru>
2018-07-26 20:42:48 +0500

    исправил код в респонс креаторе

--- response_creator.py
+++ response_creator.py
@@ -1,10 +1,10 @@
-import vk_api
+import vk
 import os
 import importlib
 from command_system import command_list
 
-vk = vk_api.VkApi(token='a6ce7f02770d6f1c1ccbfc042bd349f9b208338ad1a2314612f5790e51e2f4e085fb6baf45f8f43857f4d')
 
+token = 'a6ce7f02770d6f1c1ccbfc042bd349f9b208338ad1a2314612f5790e51e2f4e085fb6baf45f8f43857f4d'
 
 
 def load_modules():
@@ -16,9 +16,12 @@ def load_modules():
 
 def send_message(user_id, user_message):
     '''отправляет сообщение юзеру'''
+    session = vk.Session()
+    api = vk.API(session, v=5.50)
     load_modules()
     msg = create_message(user_id, user_message)
-    vk.method('messages.send', {'user_id': user_id, 'message': msg})
+    api.messages.send(access_token=token, user_id=str(user_id), message= msg)
+
 
 def create_message(user_id, user_message):
     '''ищет сообщение юзера в списке команд, в зависимости от того, что он написал,
