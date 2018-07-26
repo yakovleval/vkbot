5090dc3044440fe1c4c40691ebc01e8a852dafd5
alex <sanya89_muravlenko@mail.ru>
2018-07-26 20:33:45 +0500

    добавил обработку запросов на сервере на основного бота

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import requests
 from response_creator import send_message
+from flask import Flask, request, json
+import vk
 
 
+app = Flask(__name__)
 
+
+token = 'a6ce7f02770d6f1c1ccbfc042bd349f9b208338ad1a2314612f5790e51e2f4e085fb6baf45f8f43857f4d'
+confirmation_token = '6071d7c2'
+
+@app.route('/', methods=['POST'])
+def processing():
+    #Распаковываем json из пришедшего POST-запроса
+    data = json.loads(request.data)
+    #Вконтакте в своих запросах всегда отправляет поле типа
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        session = vk.Session()
+        api = vk.API(session, v=5.50)
+        user_id = data['object']['user_id']
+        user_message = data['object']['body']
+        send_message(user_id, user_message)  # отсылает ответ
+        # Сообщение о том, что обработка прошла успешно
+        return 'ok'
+
+'''
 #получаем с LongPoll сервера key, server и ts
 token = 'a6ce7f02770d6f1c1ccbfc042bd349f9b208338ad1a2314612f5790e51e2f4e085fb6baf45f8f43857f4d'
 data = requests.get('https://api.vk.com/method//messages.getLongPollServer', params={'v': '5.80','access_token': token}).json()['response']
@@ -19,3 +45,4 @@ while True:
 
 
     data['ts'] = response['ts']#обновление ts-a
+'''
